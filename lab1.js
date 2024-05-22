cc0fa313a04d8f42fe596d7134c8a27157f0d624
nik <nik@mail.ru>
2024-05-22 21:51:37 +0300

    lab 1 last fix

--- lab1.js
+++ lab1.js
@@ -1,52 +1,82 @@
 const assert = require('assert');
-const { Builder, By } = require('selenium-webdriver');
+const { Builder, Browser, By} = require('selenium-webdriver');
 
-let driver = new Builder().forBrowser('chrome').build();
+let driver = new Builder().forBrowser(Browser.CHROME).build();
+
+let total = 5, 
+    remaining = 5; 
 
 async function runTests() {
-  try {
-    await driver.get("https://lambdatest.github.io/sample-todo-app/");
-    await driver.manage().window().maximize();
-    await driver.sleep(1000);
-    
-    const title = await driver.getTitle();
-    assert.equal(title, "Sample page - lambdatest.com");
+    try {
+        await driver.get("https://lambdatest.github.io/sample-todo-app/");
 
-    let textElement = await driver.findElement(By.xpath("//span[contains(@class, 'ng-binding')]"));
-    let text = await textElement.getText();
-    assert.equal(text, "5 of 5 remaining");
+        await driver.manage().window().maximize();
 
-    let firstListItem = await driver.findElement(By.xpath("//ul/li[1]"));
-    let firstItemClass = await firstListItem.getAttribute("class");
-    assert.equal(firstItemClass.includes("done-true"), false);
+        await driver.sleep(2000);
 
-    await driver.findElement(By.xpath("//ul/li[1]/input")).click();
+        let header = await driver.findElement(By.className("ng-binding"));
+        let headerText = await header.getText();
 
-    firstItemClass = await firstListItem.getAttribute("class");
-    assert.equal(firstItemClass.includes("done-false"), false);
+        assert.equal(headerText, "5 of 5 remaining");
 
-    for (let i = 1; i <= 5; i++) {
-      let listItem = await driver.findElement(By.xpath(`//ul/li[${i}]`));
-      await driver.findElement(By.xpath(`//ul/li[${i}]/input`)).click();
-      let itemClass = await listItem.getAttribute("class");
-      assert.equal(itemClass.includes("done-false"), false);
+
+
+        await driver.sleep(1000);
+
+
+        for (let i = 1; i <= total; i++) {
+
+            let textElement = driver.findElement(By.xpath("//span[@class='ng-binding']"));
+            let text = await textElement.getText();
+
+            let expectedText = `${remaining} of ${total} remaining`;
+            assert.equal(text, expectedText);
+
+            let item = await driver.findElement(By.xpath(`//input[@name='li${i}']/following-sibling::span`));
+
+            let itemClass = await item.getAttribute("class");
+            assert.equal(itemClass, "done-false");
+
+            await driver.findElement(By.name("li" + i)).click();
+            remaining--;
+
+            await driver.sleep(1000);
+
+            itemClass = await item.getAttribute("class");
+            assert.equal(itemClass, "done-true");
+        } 
+
+        await driver.findElement(By.id("sampletodotext")).sendKeys("Some new cool value");
+        await driver.sleep(1000);
+
+        await driver.findElement(By.id("addbutton")).click();
+
+        let item = await driver.findElement(By.xpath("//input[@name='li6']/following-sibling::span"));
+        let itemText = await item.getText();
+        let itemClass = await item.getAttribute("class");
+
+        assert.equal(itemText, "Some new cool value");
+        assert.equal(itemClass, "done-false");
+
+        await driver.sleep(1000);
+
+        await driver.findElement(By.name("li6")).click();
+
+        itemClass = await item.getAttribute("class");
+        assert.equal(itemClass, "done-true");
+
+        await driver.sleep(3000);
+
+    } catch(err) {
+
+        driver.takeScreenshot().then(function (image) {
+            require("fs").writeFileSync('screenshot.jpg', image, 'base64')
+        })
+
+        console.error("Тест упал по причине %s", err);
+    } finally {
+        await driver.quit();
     }
-
-    await driver.findElement(By.id("sampletodotext")).sendKeys("New Item");
-    await driver.findElement(By.id("addbutton")).click();
-
-    let newItem = await driver.findElement(By.xpath("//ul/li[6]"));
-    await newItem.click();
-
-    console.log('All steps executed successfully');
-  } catch (err) {
-    await driver.takeScreenshot().then(function (image) {
-      require('fs').writeFileSync('screenshot_error.png', image, 'base64');
-    });
-    console.error('Error executing the test: %s', err);
-  } finally {
-    await driver.quit();
-  }
 }
 
 runTests();
